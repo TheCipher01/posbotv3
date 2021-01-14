87ff1c624cf5e9fe517415b9d1acc55025b3b0f8
TheCipher01 <jdmayer97@gmail.com>
2021-01-14 01:30:56 -0600

    Tried again with ticket command, no luck on getting permissions to set or even get a category made. This will probably end up getting scrapped

--- src/tickets/ticket.java
+++ src/tickets/ticket.java
@@ -6,12 +6,14 @@
 package tickets;
 
 import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Category;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.restaction.ChannelAction;
 import posv3.main;
 
 /**
@@ -34,9 +36,9 @@ public class ticket extends ListenerAdapter{
                 long userId = member.getIdLong();
                 long allow = Permission.MESSAGE_READ.getRawValue() & Permission.MESSAGE_WRITE.getRawValue();
                 long deny = Permission.MESSAGE_MENTION_EVERYONE.getRawValue();
-                event.getGuild().createTextChannel(channelName)
-                        .addMemberPermissionOverride(userId, allow, deny)
-                        .queue();
+                ChannelAction<Category> category = event.getGuild().createCategory("ticket" + channelName);
+                ChannelAction ticket = event.getGuild().createTextChannel(channelName);
+                ticket.setParent(ctgr)
             }
         }
         
