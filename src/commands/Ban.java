28866737688d41a26dc8dddfe21df88aaf04a242
TheCipher01 <jdmayer97@gmail.com>
2021-06-12 21:31:08 -0500

    Update Ban.java
    Got banid working properly now. too lazy to integrate into one command. Need to fix the regular ban command to check if you're trying to ban yourself.

--- src/commands/Ban.java
+++ src/commands/Ban.java
@@ -48,9 +48,10 @@ public class Ban extends ListenerAdapter{
                             reason += args[i] + " ";
                         }
                         log(target, event.getMember(), reason, event.getGuild().getTextChannelsByName("staff-log", true).get(0));
-                        event.getChannel().sendMessage(tMember + " was just banned by " +  staff + " for: " + reason).queue();
+                        plog(target, event.getMember(), reason, event.getChannel());
                     }else{
                         log(target, event.getMember(), "No reason provided", event.getGuild().getTextChannelsByName("staff-log", true).get(0));
+                        plog(target, event.getMember(), "No reason provided", event.getChannel());
                     }
                 }
             }
@@ -67,21 +68,25 @@ public class Ban extends ListenerAdapter{
                     if(args[1].equalsIgnoreCase(event.getAuthor().getId())){
                         event.getChannel().sendMessage("You cannot ban yourself!").queue();
                     }else{
-                        String staff = event.getMember().getAsMention();
+//                        String staff = event.getMember().getAsMention();
                         event.getGuild().ban(args[1], 0).queue();
-                        Member target2 = (Member) event.getJDA().retrieveUserById(args[1]);
-                        String t2 = target2.getAsMention();
+                        User u = User.fromId(args[1]);
+                        
                         
                         if(args.length >=3){
                             String reason = "";
                             for(int i =2; i< args.length; i++){
                                 reason += args[i] + " ";
                             }
-                            log(target2, event.getMember(), reason, event.getGuild().getTextChannelsByName("staff-log", true).get(0));
-                            event.getChannel().sendMessage(t2 + " was just banned by " +  staff + " for: " + reason).queue();
+                            event.getGuild().ban(args[1], 0, reason).queue();
+                            idlog(u, event.getMember(), reason, event.getGuild().getTextChannelsByName("staff-log", true).get(0));
+                            plogID(u, event.getMember(), reason, event.getChannel());
+                            event.getMessage().delete().queue();
                         }else{
-                            log(target2, event.getMember(), "No reason provided", event.getGuild().getTextChannelsByName("staff-log", true).get(0));
-                            event.getChannel().sendMessage(t2 + " was just banned by " +  staff + " for no reason provided").queue();
+                            event.getGuild().ban(args[1], 0, "No reason Provided. Banned By: " + event.getAuthor().getName() + ".").queue();
+                            idlog(u, event.getMember(), "No reason provided", event.getGuild().getTextChannelsByName("staff-log", true).get(0));
+                            plogID(u, event.getMember(), "No reason provided", event.getChannel());
+                            event.getMessage().delete().queue();
                         }
                     }
                 }
@@ -126,6 +131,49 @@ public class Ban extends ListenerAdapter{
         error.setDescription("{} = Required [] = Optional");
         error.addField("Proper usage: --ban {@user} [reason]", "", false);
         channel.sendMessage(error.build()).complete().delete().queueAfter(15, TimeUnit.SECONDS);
-    }        
+    }
+    /**
+     * 
+     * @param banned
+     * @param staff
+     * @param reason
+     * @param channel 
+     */
+        public void idlog(User banned, Member staff, String reason, TextChannel channel){
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        EmbedBuilder log = new EmbedBuilder();
+        log.setTitle("Ban Report");
+        log.setColor(Color.decode("#EA2027"));
+        log.addField("Banned User", banned.getAsMention(), false);
+        log.addField("Banned by", staff.getAsMention(), false);
+        log.addField("Reason", reason, false);
+        log.addField("Date", sdf.format(date), false);
+        log.addField("Time", stf.format(date), false);
+        channel.sendMessage(log.build()).queue();
+    }
+        public void plogID(User banned, Member staff, String reason, TextChannel channel){
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        EmbedBuilder log = new EmbedBuilder();           
+        log.setTitle("User Banned");
+        log.setColor(Color.decode("#EA2027"));
+        log.setDescription(banned.getAsMention() + " has been **banned** from the server by " + staff.getAsMention());
+        log.addField("Reason", reason, false);
+        channel.sendMessage(log.build()).queue();
+        }
+        public void plog(Member banned, Member staff, String reason, TextChannel channel){
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
+        SimpleDateFormat stf = new SimpleDateFormat("HH:mm:ss");
+        Date date = new Date(System.currentTimeMillis());
+        EmbedBuilder log = new EmbedBuilder();           
+        log.setTitle("User Banned");
+        log.setColor(Color.decode("#EA2027"));
+        log.setDescription(banned.getAsMention() + " has been **banned** from the server by " + staff.getAsMention());
+        log.addField("Reason", reason, false);
+        channel.sendMessage(log.build()).queue();
+        }        
     
 }
