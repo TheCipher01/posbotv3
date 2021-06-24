41477fbf55ba2d42352f6e45185febebda216181
TheCipher01 <jdmayer97@gmail.com>
2021-06-24 13:16:14 -0500

    Update buttonTest.java

--- src/commands/buttonTest.java
+++ src/commands/buttonTest.java
@@ -29,7 +29,7 @@ public class buttonTest extends ListenerAdapter{
         if(args[0].equalsIgnoreCase(main.prefix + "au2")){
             event.getChannel().sendMessage("Click the buttons to either yourself the Among Us Role or to Remove it!")
                     .setActionRow(Button.success("give", "Get the among us role!"))
-                    .setActionRow(Button.danger("remove", "Remove the among us role!"))
+                    //.setActionRow(Button.danger("remove", "Remove the among us role!"))
                     .queue();
         }
     }
@@ -41,15 +41,13 @@ public class buttonTest extends ListenerAdapter{
             if(!member.getRoles().contains(role)){
                 event.getChannel().sendMessage("You have been added to the Among Us Role!").queue();
                 event.getGuild().addRoleToMember(member, role).complete();
-            }else{
-                return;
             }
-        }else if(event.getComponentId().equals("remove")){
-            Member member = event.getMember();
-            Role role = event.getGuild().getRoleById("760705745582293013");            
-            event.getChannel().sendMessage("You have been removed from the Among Us Role!").queue();
-            event.getGuild().removeRoleFromMember(member, role).complete();
-        }
+//        }else if(event.getComponentId().equals("remove")){
+//            Member member = event.getMember();
+//            Role role = event.getGuild().getRoleById("760705745582293013");            
+//            event.getChannel().sendMessage("You have been removed from the Among Us Role!").queue();
+//            event.getGuild().removeRoleFromMember(member, role).complete();
+//        }
     }
     
 }
