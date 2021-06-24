f864b8fefb449d9c9add3b63fa6ae71691e0a09b
TheCipher01 <jdmayer97@gmail.com>
2021-06-24 13:28:31 -0500

    Update buttonTest.java
    Set up currently so that the command can be run and the message with the buttons will stay forever so people don't have to use commands to get or remove themselves from roles.

--- src/commands/buttonTest.java
+++ src/commands/buttonTest.java
@@ -30,6 +30,7 @@ public class buttonTest extends ListenerAdapter{
             event.getChannel().sendMessage("Click the buttons to either yourself the Among Us Role or to Remove it!")
                     .setActionRow(Button.success("give", "Get the Among Us Role!"), Button.danger("remove", "Remove the Among Us Role!"))
                     .queue();
+            event.getMessage().delete().queue();
         }
     }
     @Override
@@ -37,15 +38,19 @@ public class buttonTest extends ListenerAdapter{
         if(event.getComponentId().equals("give")){
             Member member = event.getMember();
             Role role = event.getGuild().getRoleById("760705745582293013");
+            String name = member.getAsMention();
             if(!member.getRoles().contains(role)){
-                event.getChannel().sendMessage("You have been added to the Among Us Role!").queue();
+                event.getChannel().sendMessage(name + ", You have been added to the Among Us Role!").queue();
                 event.getGuild().addRoleToMember(member, role).complete();
+                //event.getMessage().delete().queue();
             }
         }else if(event.getComponentId().equals("remove")){
             Member member = event.getMember();
-            Role role = event.getGuild().getRoleById("760705745582293013");            
-            event.getChannel().sendMessage("You have been removed from the Among Us Role!").queue();
+            Role role = event.getGuild().getRoleById("760705745582293013");
+            String name = member.getAsMention();
+            event.getChannel().sendMessage(name + ", You have been removed from the Among Us Role!").queue();
             event.getGuild().removeRoleFromMember(member, role).complete();
+            //event.getMessage().delete().queue();
         }
     }
     
